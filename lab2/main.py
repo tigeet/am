e8edaa1a350d0695f360f9bd0e47ad7c8d08e587
tigeet <tigeeet@gmail.com>
2023-11-20 20:31:20 +0300

    lab2

---
+++ lab2/main.py
@@ -0,0 +1,143 @@
+import numpy as np
+from simplex import solve
+from pathlib import Path
+import json
+
+
+def simplify_rows(A):
+    A = A.copy()
+
+    def helper():
+        for i, _ in enumerate(A):
+            for j, _ in enumerate(A):
+                if i == j:
+                    continue
+
+                if np.all(A[i] >= A[j]):
+                    return True, np.delete(A, j, axis=0)
+        return False, A
+
+    can_be_simplified = True
+    while can_be_simplified:
+        can_be_simplified, A = helper()
+    return A
+
+
+def simplify_cols(A):
+    A = A.copy()
+
+    def helper(A):
+        for i, _ in enumerate(A[0]):
+            for j, _ in enumerate(A[0]):
+                if i == j:
+                    continue
+
+                if np.all(A[:, i] <= A[:, j]):
+                    return True, np.delete(A, j, axis=1)
+        return False, A
+
+    can_be_simplified = True
+
+    while can_be_simplified:
+        can_be_simplified, A = helper(A)
+
+    return A
+
+
+def simplify(A):
+    A = simplify_rows(A)
+    A = simplify_cols(A)
+    return A
+
+
+def boundary(A):
+    l_s = np.argmax([min(row) for row in A])
+    u_s = np.argmin([max(col) for col in A.T])
+
+    l = min(A[l_s])
+    u = max(A.T[u_s])
+
+    return l, u
+
+
+def from_json(path: Path):
+    file = open(path.absolute(), "r", encoding="utf-8")
+    content = file.read()
+    content = json.loads(content)
+    a = content["matrix"]
+    _json = [[float(c) for c in row] for row in a]
+
+    file.close()
+    return np.array(_json)
+
+
+path = Path("./assets/input2.json")
+
+
+def resize(arr, ln):
+    return arr + [0] * (ln - len(arr))
+
+
+def main():
+    # A = from_json(path)
+    # A = [[2, 5, 8], [7, 4, 3]]  # решение 4.5, A = [0.5, 0.5], B = [0.17, 0.83]
+
+    # A = [
+    #     [1, 4],
+    #     [3, 1],
+    #     [2, 2.5],
+    #     [5, 0],
+    # ]  # v = 2.5, A = [0.625, 0.375], B = [0.5, 0.5]
+
+    # A = [
+    #     [1, 4, 6, 3, 7],
+    #     [3, 1, 2, 4, 3],
+    #     [2, 3, 4, 3, 5],
+    #     [0, 1, 5, 2, 6],
+    # ]  # v = 7/3, A = [2/3, 1/3, 0, 0, 0], B = [0, 1/3, 2/3, 0]
+
+    A = [
+        [4, 2, 3, -1],
+        [-4, 0, -2, 2],
+    ]  # v = 4/11, A = [6/11, 5/11], B = [3/11, 0, 0 ,8/11]
+
+    A = np.array(A)
+    len_a = len(A)
+    len_b = len(A[0])
+
+    a = simplify(A)
+    print(a)
+    l, u = boundary(a)
+
+    b = np.ones(len(a)).T
+    c = np.ones(len(a[0]))
+
+    solution_b, value_b = solve(a, b, c)
+    solution_b = [y / value_b for y in solution_b]
+    value = 1 / value_b
+
+    a = -a.T
+    b = -np.ones(len(a)).T
+    c = -np.ones(len(a[0]))
+
+    solution_a, value_a = solve(a, b, c)
+    value_a = -value_a
+    solution_a = [x / value_a for x in solution_a]
+
+    solution_a = resize(solution_a, len_a)
+    solution_b = resize(solution_b, len_b)
+
+    if l == u:
+        print("Игра имеет решение в чистых стратегиях")
+        return
+    else:
+        print(f"{l} < v < {u}")
+        print(f"Цена игры = {value}")
+        print(f"Стратегии игрока A = {[str(round(x, 2)) for x in solution_a]}")
+        print(f"Стратегии игрока B = {[str(round(y, 2)) for y in solution_b]}")
+        print("")
+        print(f"Стратегии игрока A = {[str(x) for x in solution_a]}")
+        print(f"Стратегии игрока B = {[str(y) for y in solution_b]}")
+
+
+main()
---
+++ lab2/simplex.py
@@ -0,0 +1,175 @@
+import json
+from pathlib import Path
+import numpy as np
+
+with_log = False
+
+
+def parse_json(content: str):
+    doc = json.loads(content)
+    C = set()
+    constraints = doc["constraints"]
+    height = len(constraints)
+    width = len(constraints[0]["coefs"])
+
+    f = np.array(doc["f"])
+
+    if doc["goal"] == "min":
+        f *= -1
+
+    A = np.empty(0)
+    B = np.empty(0)
+
+    for i, constraint in enumerate(constraints):
+        row_len = len(constraint["coefs"])
+
+        coefs = np.zeros(width)
+        coefs[:row_len] = np.array(constraint["coefs"])
+
+        b = constraint["b"]
+
+        if constraint["type"][0:2] == "gt":
+            b = -b
+            coefs *= -1
+        if constraint["type"][0:2] == "eq":
+            C.add(i)
+        A = np.append(A, coefs)
+        B = np.append(B, b)
+
+    A = np.reshape(A, (height, width))
+    B = np.reshape(B, (1, height))
+
+    return A, B[0], f, C
+
+
+def from_file(path: Path):
+    file = open(path.absolute(), "r", encoding="utf-8")
+    content = file.read()
+    file.close()
+    return content
+
+
+def make_tableau(A, b, c, C):
+    tableau = np.vstack((A, -c))
+
+    identity = np.identity(tableau.shape[0])
+    for i, _ in enumerate(identity):
+        if i in C:
+            identity[i][i] = 0
+
+    tableau = np.hstack((tableau, identity))
+    b = np.append(b, 0)
+    tableau = np.hstack((tableau, np.atleast_2d(b).T))
+
+    return tableau
+
+
+def get_pivot(tableau):
+    if np.any(tableau[:-1, -1] < 0):
+        target_row = 0
+        for i, v in enumerate(tableau[:-1, -1]):
+            if v < 0:
+                target_row = i
+                break
+
+        pivot_col = None
+        for i, v in enumerate(tableau[target_row]):
+            if v < 0:
+                pivot_col = i
+                break
+
+        if pivot_col is None:
+            return -1, -1
+
+        f = tableau[:-1, -1] / tableau[:-1, pivot_col]
+        f = [v if v > 0 else np.inf for v in f]
+        pivot_row = np.argmin(f)
+        if f[pivot_row] == np.inf:
+            return -1, -1
+        return pivot_row, pivot_col
+        # for i, v in enumerate(tableau[pivot_row][:-1]):
+        #     if v < 0:
+        #         return pivot_row, i
+
+        return -1, -1
+    else:
+        height = len(tableau - 1)
+        pivot_column = np.argmin(tableau[-1][:-1])
+        nonneg = [i for i in range(0, height) if tableau[i, pivot_column] > 0]
+        if len(nonneg) == 0:
+            return None, None
+
+        division = [tableau[i][-1] / tableau[i][pivot_column] for i in nonneg]
+        pivot_row = nonneg[division.index(min(division))]
+        return pivot_row, pivot_column
+
+
+def solve(A, b, c, C=set()):
+    tableau = make_tableau(A, b, c, C)
+    if with_log:
+        print(tableau, "\n------")
+    # print(tableau)
+    height = len(A)
+    width = len(c)
+
+    optimal = False
+    feasible = True
+
+    while feasible and not optimal:
+        # for i in range(5):
+        pivot_row, pivot_column = get_pivot(tableau)
+        if pivot_column is None:
+            feasible = False
+            # print("unbounded")
+            break
+        elif pivot_column == -1:
+            feasible = False
+            # print("infeasible")
+            break
+
+        tableau[pivot_row] /= tableau[pivot_row][pivot_column]
+
+        for i in range(height + 1):
+            if i == pivot_row:
+                continue
+            tableau[i] -= (
+                tableau[i][pivot_column]
+                * tableau[pivot_row]
+                / tableau[pivot_row][pivot_column]
+            )
+        if with_log:
+            print(tableau, "\n------")
+        solutions = tableau[:, -1]
+
+        optimal = np.all(tableau[:-1, -1] >= 0) and np.all(tableau[-1][:-1] >= 0)
+
+    if optimal:
+        solutions = np.zeros(width)
+        has_solution = False
+        for i in range(width):
+            if sum(tableau[:, i]) != 1:
+                solutions[i] = 0
+            else:
+                has_solution = True
+                solutions[i] = tableau[tableau[:, i].argmax(), -1]
+        value = tableau[-1, -1]
+        if has_solution:
+            if with_log:
+                print("Simplex solution:", solutions, value)
+            return solutions, value
+    raise "no solution"
+
+
+def main():
+    # path = Path("./assets/input7.json")
+    # content = from_file(path)
+    # A, b, c, C = parse_json(content)
+    A = np.array([[1, 1], [-2, 3]])
+    b = np.array([13, -6])
+    c = np.array([1, 2])
+    C = set([0])
+    solve(A=A, b=b, c=c, C=C)
+
+
+if __name__ == "__main__":
+    main()
